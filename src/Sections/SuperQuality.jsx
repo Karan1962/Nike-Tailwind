9ba680f18b86c44651e09800ee565829b3d4494e
karan <karan@gmail.com>
2023-08-21 02:10:24 +0530

    services superquality popularproductcard done

--- src/Sections/SuperQuality.jsx
+++ src/Sections/SuperQuality.jsx
@@ -1,7 +1,34 @@
+import { shoe8 } from "../assets/images";
+import Button from "../components/Button";
+
 const SuperQuality = () => {
   return (
-    <div>SuperQuality</div>
-  )
-}
+    <section
+      id="aboutUs"
+      className="flex justify-between items-center max-lg:flex-col gap-10 w-full"
+    >
+      <div className="flex flex-1 flex-col">
+        <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+          We Provide You
+          <span className="text-coral-red "> Super</span>
+          <span className="text-coral-red "> Quality </span>
+           Shoes
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Ensuring premium comfort and style, our meticulously crafted footwear
+          is designed to elevate your experience, providing you with unmatched
+          quality, innovation, and a touch of elegance.
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">Our dedication to detail and excellence ensures your satisfaction</p>
+        <div className="mt-11">
+        <Button label="View Details" />
+        </div>
+      </div>
+      <div className="flex flex-1 justify-center items-center">
+        <img src={shoe8} alt="8thshoe"  width={570} height={522}/>
+      </div>
+    </section>
+  );
+};
 
-export default SuperQuality
+export default SuperQuality;
