67ed816988874ad3250aacddc17a2aaff78db3f5
karan <karan@gmail.com>
2023-08-20 19:39:09 +0530

    hero section complete

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import CustomNav from './components/CustomNav'
 const App = () => (
   <main className="relative">
     <CustomNav/>  
-    <section className="xl:padding-1 wide:padding-r padding-b"><Hero/></section>
+    <section className="xl:padding-l wide:padding-r padding-b"><Hero/></section>
     <section className="padding"><PopularProducts/></section>
     <section className="padding"><SuperQuality/></section>
     <section className="padding-x"><Services/></section>
--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -1,7 +1,70 @@
-const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+import { arrowRight } from "../assets/icons";
+import Button from "../components/Button";
+import ShoeCard from "../components/ShoeCard";
+import { shoes, statistics } from "../constants";
+import { bigShoe1 } from "../assets/images";
+import { useState } from "react";
 
-export default Hero
+const Hero = () => {
+  const [bigShoeImg , setBigShoeImg] = useState(bigShoe1)
+  return (
+    <section
+      id="home"
+      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
+    >
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+        <p className="text-xl font-monsorrat text-coral-red">
+          Our summer Collection
+        </p>
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
+            The New arrival
+          </span>
+          <br />
+          <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
+        </h1>
+        <p className="font-monsorrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
+          Discover stylish Nike arrivals, quality comfort, and innovation for
+          your active life.
+        </p>
+        <Button label="shop now" iconURL={arrowRight} />
+        <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
+          {statistics.map((stats) => (
+            <div key={stats.label}>
+              <p className="text-4xl font-palanquin font-bold">{stats.value}</p>
+              <p className="leading-7 font-montserrat text-slate-gray">
+                {stats.label}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
+        <img
+          src={bigShoeImg}
+          alt="bigshoe"
+          width={610}
+          height={500}
+          className="object-contain relative z-10"
+        />
+
+        <div>
+          <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
+            {shoes.map((shoe, index) => (
+              <div key={shoe}>
+                <ShoeCard
+                  imgURL={shoe}
+                  changeBigShoeImage={(shoe) => setBigShoeImg(shoe)}
+                  bigShoeImg={bigShoeImg}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
